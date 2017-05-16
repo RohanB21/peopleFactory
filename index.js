55b5a18886e605e14878254dc67076b44745ef7c
Rohan <rbalel@purdue.edu>
2017-05-16 09:45:50 -0400

    Add haircolor as a box

---
+++ index.js
@@ -0,0 +1,54 @@
+const personForm = document.querySelector('form')
+
+const renderColor = (hairColor) => {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = hairColor
+  colorDiv.style.height = '50px'
+  colorDiv.style.width = '100px'
+  return colorDiv
+}
+
+const handleSubmit = (ev) => {
+  ev.preventDefault()
+  const form = ev.target
+  const details = document.querySelector('.details')
+
+  const personName = form.personName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
+
+  const colorDiv = renderColor(hairColor)
+
+  const overall = document.createElement('ul')
+  const nameVar = document.createElement('li')
+  const hairVar = document.createElement('li')
+  const ageVar = document.createElement('li')
+  const birthVar = document.createElement('li')
+
+  nameVar.textContent = 'Name: ' + personName
+  ageVar.textContent = 'Age: ' + age
+  birthVar.textContent = 'Birth Place: ' + birthplace 
+  hairVar.textContent = 'Hair Color: '
+  
+  hairVar.appendChild(colorDiv)
+  overall.appendChild(nameVar)
+  overall.appendChild(hairVar)
+  overall.appendChild(ageVar)
+  overall.appendChild(birthVar)
+  details.appendChild(overall)
+
+  
+  
+  //details.innerHTML = `
+    //<ul>
+      //<li>Name: ${personName}</li>
+      //<li>Hair Color: ${colorDiv.outerHTML}</li>
+      //<li>Age: ${age}</li>
+      //<li>Birthplace: ${birthplace}</li>
+    //</ul>
+  //`
+
+}
+
+personForm.addEventListener('submit', handleSubmit)

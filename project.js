b6bfb64b618a695e4ba74f8e5b955a9a2be44e85
Rohan <rbalel@purdue.edu>
2017-05-17 09:56:55 -0400

    Final touches basically insignificant

--- project.js
+++ project.js
@@ -6,11 +6,13 @@ const handleSubmit = (ev) => {
   const details = document.querySelector('.details')
   const personName = form.personName.value
   const overall = document.createElement('ul')
+  
   const nameVar = document.createElement('li')
   nameVar.textContent = personName
-  
+
   overall.appendChild(nameVar)
   details.appendChild(overall)
+ 
   form.reset()
 }
 personForm.addEventListener('submit', handleSubmit)
